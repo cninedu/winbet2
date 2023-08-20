1305041f55685189aaa8dcd70e572150ac7f6989
cninedu <128992847+cninedu@users.noreply.github.com>
2023-08-19 17:00:11 -0700

    second commit
    Signed-off-by: cninedu <128992847+cninedu@users.noreply.github.com>

---
+++ apple.js
@@ -0,0 +1,42 @@
+
+const divContainer =document.querySelector('#elementToworkon');
+
+let isClicked =true;
+
+
+let showOrHide =function(){
+
+if(isClicked){
+
+    divContainer.style.display ='block';
+isClicked =false;   
+    
+}else{
+
+    divContainer.style.display ='none';
+isClicked =true; 
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
